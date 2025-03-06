a16658a579f5ae3b5d86695f0d4eda4f341a5307
amk3y <37068498+amk3y@users.noreply.github.com>
2025-03-07 01:58:10 +0800

    lab 2

--- lab2_oled/MDK-ARM/lab2_oled_example/main.d
+++ lab2_oled/MDK-ARM/lab2_oled_example/main.d
@@ -33,6 +33,7 @@ lab2_oled_example/main.o: ..\Core\Src\main.c ..\Core\Inc\main.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h \
   ..\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_uart.h \
+  ..\Core\Inc\ghast.h ..\Core\Inc\ghast_fire.h \
   ..\Middlewares\Third_Party\FreeRTOS\Source\CMSIS_RTOS_V2\cmsis_os.h \
   ..\Middlewares\Third_Party\FreeRTOS\Source\include\FreeRTOS.h \
   ..\Core\Inc\FreeRTOSConfig.h \
